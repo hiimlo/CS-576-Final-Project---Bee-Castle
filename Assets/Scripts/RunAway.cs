5d46825e20c23c4f26e17c7923f941b299d2b8c8
hiimlo <hazarikalola@gmail.com>
2022-12-11 23:05:20 -0500

    runaway code for now

---
+++ Assets/Scripts/RunAway.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunAway : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Vector3 normDir = (player.position - npc.position).normalized;
+
+        // normDir = Quaternion.AngleAxis(Random.Range(0, 89), Vector3.up) * normDir;
+
+        // Vector3 move = transform.position - (normDir * 10f);
+        // player.SetDestination(move);
+        // player.isStopped = false;
+        
+    }
+}
