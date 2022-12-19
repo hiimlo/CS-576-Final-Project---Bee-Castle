eaa20dd08c4f3167e70ffcd1e0875fda8238f341
hiimlo <hazarikalola@gmail.com>
2022-12-19 00:08:28 -0500

    finished flower parts, collision works and updates the inventory

---
+++ Assets/Scripts/FlowerPart.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerPart : MonoBehaviour
+{
+    private GameSystem gs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject gs_obj = GameObject.FindGameObjectWithTag("GameSystem");
+        gs = gs_obj.GetComponent<GameSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f, 30f * Time.deltaTime, 0f, Space.Self);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Capsule")
+        {
+            if (gameObject.name == "petal"){
+                gs.foundPetals = true;
+            }
+            else if (gameObject.name == "pistil") {
+                gs.foundPistil = true;
+            }
+            else if (gameObject.name == "stamen") {
+                gs.foundStamen = true;
+            }
+            else if (gameObject.name == "ovules") {
+                gs.foundOvules = true;
+            }
+            else if (gameObject.name == "sepals") {
+                gs.foundSepals = true;
+            }
+
+            //gs.source.PlayOneShot(gs.flowersound, 0.5f); temp code, add in once sounds are added
+            Destroy(gameObject);
+        }
+    }
+}
